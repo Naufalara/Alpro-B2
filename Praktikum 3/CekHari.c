c431258f30f1401d6ec47bacc692be85ed45ee79
NaufalAryaRamadhan <100133797+Naufalara@users.noreply.github.com>
2022-03-13 21:46:12 +0700

    Create CekHari.c

---
+++ Praktikum 3/CekHari.c
@@ -0,0 +1,44 @@
+/*Nama file : CekHari*/
+/*Pembuat   : 24060121110042-Naufal Arya Ramadhan*/
+/*Deskripsi : Menuliskan nama hari berdasarkan input angka m yang dimasukkan*/
+/*Tanggal   : Senin, 7 Maret 2022*/
+
+#include<stdio.h>
+/*
+int main(){
+    //kamus
+    int a;
+    //algoritma
+    printf("Program menuliskan nama hari \n")
+    printf("Masukkan nomor hari : ");
+    scanf("%d", &a);
+    switch(a){
+case 1 :
+    printf("senin");
+    break;
+case 2 :
+    printf("selasa");
+    break;
+case 3:
+    printf("Rabu");
+    break;
+case 4:
+    printf("Kamis");
+    break;
+case 5:
+    printf("Jumat");
+    break;
+case 6:
+    printf("Sabtu");
+    break;
+case 7:
+    printf("Minggu");
+    break;
+default :
+    printf("Masukan nomor hari tidak tepat");
+    break;
+    }
+    return 0;
+}
+
+*/
