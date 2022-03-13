6111a22bf7fa2839593a37eb107b891eb9363a54
NaufalAryaRamadhan <100133797+Naufalara@users.noreply.github.com>
2022-03-13 21:45:45 +0700

    Create Cekbulan.c

---
+++ Praktikum 3/Cekbulan.c
@@ -0,0 +1,58 @@
+/*Nama file : CekBulan*/
+/*Pembuat   : 24060121110042-Naufal Arya Ramadhan*/
+/*Deskripsi : Menuliskan nama bulan berdasarkan input angka d yang dimasukkan*/
+/*Tanggal   : Senin, 7 Maret 2022*/
+
+#include<stdio.h>
+/*
+int main(){
+    //kamus
+    int m;
+    //algoritma
+    printf("Program menuliskan nama bulan \n");
+    printf("masukkan nomor bulan : ");
+    scanf("%d",&m);
+    switch(m){
+case 1 :
+    printf("Januari");
+    break;
+case 2 :
+    printf("Februari");
+    break;
+case 3:
+    printf("Maret");
+    break;
+case 4:
+    printf("April");
+    break;
+case 5:
+    printf("Mei");
+    break;
+case 6:
+    printf("Juni");
+    break;
+case 7:
+    printf("Juli");
+    break;
+case 8:
+    printf("Agustus");
+    break;
+case 9:
+    printf("September");
+    break;
+case 10:
+    printf("Oktober");
+    break;
+case 11:
+    printf("November");
+    break;
+case 12:
+    printf("Desember");
+    break;
+default :
+    printf("Masukan nomor bulan tidak tepat");
+    break;
+    }
+    return 0;
+}
+*/
