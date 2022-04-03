6842f3a9755f84fb0bbeb2ab067f4c3ca9862e6e
NaufalAryaRamadhan <100133797+Naufalara@users.noreply.github.com>
2022-04-03 22:46:05 +0700

    Create tetris.c

---
+++ Praktikum 6/tetris.c
@@ -0,0 +1,29 @@
+/*Nama File 	: tetris*/
+/*Deskripsi 	: Diberikan sebuah integer N > 0 yang dimasukan melalui keyboard, maka  tampilkanlah dilayar susunan karakter ‘*’*/
+/*Pembuat   	: 24060121110042-Naufal Arya Ramadhan*/
+/*Tgl Pembuatan	: Minggu, 4 April 2022*/
+
+#include <stdio.h>
+int main()
+{
+ //Kamus
+   int N;
+   int i;
+   int j;
+ //Algoritma
+   system("color B");
+   printf("Ketiklah nilai N yang ingin dimasukkan = ");
+   scanf("%d", &N);
+   if (N > 0){
+    for(i = 1; i <= N; i++){
+     for(j = 1; j <= i; j++){
+      printf("* ");
+     }
+      printf("\n");
+    }
+   }
+   else{
+    printf("Pastikan bilangan integer melebihi 0 !");
+   }
+ return 0;
+}
